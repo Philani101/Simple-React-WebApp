c72f1289679df43e89eeb06cec1bca0dee41417d
Philani <philanimkalipi@gmail.com>
2025-07-10 18:58:06 +0200

    added some styling and a click event

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,11 +1,7 @@
-import './App.css';
 import Navbar from './NavBar.jsx';
 import Home from './Home.jsx';
 
 function App() {
- const title = 'welcome to the new blog'
- const likes = 50;
- const link = 'https://music.youtube.com'
 
   return (
       <div className="App">
--- my-app/src/Home.jsx
+++ my-app/src/Home.jsx
@@ -1,7 +1,13 @@
 const Home = () => {
+
+    const handleClick = (msg,e) => {
+        alert(msg)
+        console.log(e)
+    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick ={(e) => {handleClick("hello bloggers:)",e)}}>Click me</button>
         </div>
      );
 }
