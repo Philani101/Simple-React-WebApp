918d977e3070b61630a3bd65b77fd5020f7cb187
Philani <philanimkalipi@gmail.com>
2025-07-11 15:19:28 +0200

    useState

--- my-app/src/Home.jsx
+++ my-app/src/Home.jsx
@@ -1,12 +1,17 @@
+import {useState} from 'react';
+
 const Home = () => {
+    const [name,setName] = useState("N/A");
+    const [age, setAge] = useState("");
 
     const handleClick = (msg,e) => {
-        alert(msg)
-        console.log(e)
+        setName('Luige')
+        setAge(9);
     }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <p>{name} is {age} years old</p>
             <button onClick ={(e) => {handleClick("hello bloggers:)",e)}}>Click me</button>
         </div>
      );
